758260cd4cc73eaf08edb5b855193d04f131d960
abhilash4384 <vickyabhilash4384@gmail.com>
2024-06-08 00:39:48 +0530

    more docs added

--- src/stories/button/Button.stories.js
+++ src/stories/button/Button.stories.js
@@ -26,31 +26,3 @@ export const Primary = {
 		label: 'Button',
 	},
 };
-
-export const Secondary = {
-	args: {
-		label: 'Button',
-	},
-};
-
-export const Large = {
-	args: {
-		size: 'large',
-		label: 'Button',
-	},
-};
-
-export const Small = {
-	args: {
-		size: 'small',
-		label: 'Button',
-	},
-};
-
-export const Warning = {
-	args: {
-		primary: true,
-		label: 'Delete now',
-		backgroundColor: 'red',
-	},
-};
