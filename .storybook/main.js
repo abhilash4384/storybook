30db876e2589f0e907ecdae842253286f2e01366
abhilash4384 <vickyabhilash4384@gmail.com>
2024-09-08 08:39:51 +0530

    more documentation

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,22 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
-  },
+	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+	addons: [
+		'@storybook/addon-onboarding',
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+		{
+			name: '@storybook/addon-docs',
+			options: {
+				configureJSX: true,
+				category: 'JavaScript',
+			},
+		},
+	],
+	framework: {
+		name: '@storybook/react-vite',
+		options: {},
+	},
 };
 export default config;
